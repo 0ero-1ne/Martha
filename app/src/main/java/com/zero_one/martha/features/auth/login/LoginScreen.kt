c6ef3208b135f056824718dd11eb6cef10cbdc3c
0ero1ne <volikovd17@gmail.com>
2025-06-01 01:39:57 +0300

    fix(Logo): Fixed monochrome style

--- app/src/main/java/com/zero_one/martha/features/auth/login/LoginScreen.kt
+++ app/src/main/java/com/zero_one/martha/features/auth/login/LoginScreen.kt
@@ -13,10 +13,11 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
@@ -29,15 +30,13 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
-import coil3.compose.AsyncImage
-import coil3.request.ImageRequest
-import coil3.request.crossfade
 import com.zero_one.martha.R
 import com.zero_one.martha.ui.components.CustomTopBar
 import com.zero_one.martha.ui.forms.login.LoginForm
@@ -114,20 +113,12 @@ fun LoginScreen(
                     modifier = Modifier
                         .weight(1f),
                 )
-                AsyncImage(
-                    model = ImageRequest.Builder(LocalContext.current)
-                        .data(
-                            if (isSystemInDarkTheme())
-                                R.drawable.app_logo_light
-                            else R.drawable.app_logo_dark,
-                        )
-                        .crossfade(true)
-                        .build(),
+                Icon(
+                    imageVector = ImageVector.vectorResource(R.drawable.app_logo),
                     contentDescription = "App logo icon",
-                    contentScale = ContentScale.FillHeight,
+                    tint = MaterialTheme.colorScheme.onSurface,
                     modifier = Modifier
-                        .width(150.dp)
-                        .height(150.dp),
+                        .size(150.dp),
                 )
                 Spacer(
                     modifier = Modifier
